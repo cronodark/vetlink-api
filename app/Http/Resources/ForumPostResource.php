050a2aa995d2c594bae629b3bdfefa7592493776
Kevin Farhan H <kevinfarhan95@gmail.com>
2024-11-18 15:19:36 +0700

    hotfix/fix-user-photo-for-forum-response

--- app/Http/Resources/ForumPostResource.php
+++ app/Http/Resources/ForumPostResource.php
@@ -25,7 +25,7 @@ class ForumPostResource extends JsonResource
             'user' => [
                 'id' => $this->user->id,
                 'username' => $this->user->username,
-                'photo' => $this->user->photo,
+                'photo' => $this->user->photo != null ? $this->getFullUserImageUrl($this->user->photo) : null,
             ],
             // 'comments' => ForumCommentResource::collection($this->comments)
         ];
@@ -36,6 +36,17 @@ class ForumPostResource extends JsonResource
         // Check if the pet image is already a full URL
         if (filter_var($petImage, FILTER_VALIDATE_URL)) {
             return $petImage;
+        }
+
+        // Otherwise, assume it is a path in the storage and generate the full URL
+        return url('/storage/' . $this->pet_image);
+    }
+
+    private function getFullUserImageUrl(string $userImage): string
+    {
+        // Check if the pet image is already a full URL
+        if (filter_var($userImage, FILTER_VALIDATE_URL)) {
+            return $userImage;
         }
 
         // Otherwise, assume it is a path in the storage and generate the full URL
